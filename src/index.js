c6e52245b560027272e56f5e87fa725e1eb9f3ab
Edison Chee <edisonchee@live.com>
2016-10-05 15:59:26 +0800

    Add telegram bot integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import { default as restify } from 'restify';
+import { default as Slimbot } from 'slimbot';
 
+const slimbot = new Slimbot(process.env.TELEGRAM_TOKEN);
+const userId = process.env.TELEGRAM_USER_ID;
 const server = restify.createServer();
 
 server.use(restify.bodyParser());
 
 server.post('/aws', (req, res) => {
   const body = JSON.parse(req.body);
-  console.log(`Subject: ${body.subject}\nMessage: ${body.message}`);
+  const message = `Subject: ${body.subject}\nMessage: ${body.message}`;
+  slimbot.sendMessage(userId, message);
   res.send(204);
 });
 
 server.listen(7777, () => {
   console.log(`${server.name} listening at ${server.url}`);
 });
+
+slimbot.startPolling();
