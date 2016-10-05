60128816393b919abbd699670e5b5396c2d4c12a
Edison Chee <edisonchee@live.com>
2016-10-05 18:52:37 +0800

    Working prototype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { default as restify } from 'restify';
 import { default as Slimbot } from 'slimbot';
+import { default as prettysize } from 'prettysize';
 
 const slimbot = new Slimbot(process.env.TELEGRAM_TOKEN);
 const userId = process.env.TELEGRAM_USER_ID;
@@ -8,9 +9,14 @@ const server = restify.createServer();
 server.use(restify.bodyParser());
 
 server.post('/aws', (req, res) => {
-  const body = JSON.parse(req.body);
-  const message = `Subject: ${body.subject}\nMessage: ${body.message}`;
-  slimbot.sendMessage(userId, message);
+  const bodyObj = JSON.parse(req.body);
+  const msgObj = JSON.parse(bodyObj.Message);
+  const record = msgObj.Records[0];
+  const params = {
+    parse_mode: 'Markdown',
+  };
+  const message = `*Action:* ${record.eventName}\n*IP:* ${record.requestParameters.sourceIPAddress}\n*Bucket:* ${record.s3.bucket.name}\n*Object Key:* ${record.s3.object.key}\n*Object Size:* ${prettysize(record.s3.object.size)}`;
+  slimbot.sendMessage(userId, message, params);
   res.send(204);
 });
 
