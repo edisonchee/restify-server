27dc57159007e0f86de38d7611b689560018e154
Edison Chee <edisonchee@live.com>
2016-10-05 15:04:35 +0800

    Barebones server

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ const server = restify.createServer();
 server.use(restify.bodyParser());
 
 server.post('/aws', (req, res) => {
-  // send something
+  console.log(req.body);
+  res.send(204);
+});
+
+server.listen(7777, () => {
+  console.log(`${server.name} listening at ${server.url}`);
 });
